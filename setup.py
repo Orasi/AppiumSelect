6bffa6eb266aeb12cbf4ef42c6aaffc39e63fff8
mattwhooo <matt.watson@orasi.com>
2015-11-04 14:40:54 -0500

    Add ico file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'appium_selector',
   packages = ['appium_selector'],
-  version = '0.3.9',
+  version = '0.3.10',
   description = 'Creates a Tcl/Tk window to read connected Appium devices from Selenium Grid and display them for selection',
   author = 'Matt Watson',
   author_email = 'Watson.Mattc@gmail.com',
   url = 'https://github.com/Mattwhooo/appium_selector.git',
-  download_url = 'https://github.com/Mattwhooo/appium_selector/tarball/0.3.9',
+  download_url = 'https://github.com/Mattwhooo/appium_selector/tarball/0.3.10',
   keywords = ['appium', 'selenium', 'testing'],
   classifiers=[],
   install_requires=['beautifulsoup4', 'requests']
