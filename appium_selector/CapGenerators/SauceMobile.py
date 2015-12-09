d0492bd23f72306906802ae4b7543aea65344817
Matt Watson <matt.watson@orasi.com>
2015-12-09 12:10:10 -0500

    1.1.4

--- appium_selector/CapGenerators/SauceMobile.py
+++ appium_selector/CapGenerators/SauceMobile.py
@@ -21,11 +21,11 @@ class SauceMobile(Caps):
         self.options['model'] = self.env.find('deviceName').text
         self.options['osv'] = self.env.find('platformVersion').text
         self.options['mustard'] = mustard
+        self.options['deviceName'] = self.displayString().replace('.','').replace(' ', '')
 
         self.caps['deviceName'] = self.env.find('deviceName').text
         self.caps['deviceOrientation'] = self.env.find('deviceOrientation').text
         self.caps['browserName'] = ''
-        self.caps['udid'] = self.displayString().replace('.','').replace(' ', '')
         self.caps['platformVersion'] = self.env.find('platformVersion').text
         self.caps['platformName'] = self.env.find('platformName').text
 
