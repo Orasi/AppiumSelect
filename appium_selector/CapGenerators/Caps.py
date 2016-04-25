c3ca75abb3b1aed98b85ddd34a066a776b37ac0d
Matt Watson <matt.watson@orasi.com>
2016-04-25 10:14:55 -0400

    1.2.18

--- appium_selector/CapGenerators/Caps.py
+++ appium_selector/CapGenerators/Caps.py
@@ -5,4 +5,7 @@ class Caps(object):
     pass
 
 def propertyFromString(property, title):
-    return re.search(property + '=.*?[}|,]', title).group().split('=')[1].replace('}','').replace(',','')
+    try:
+        return re.search(property + '=.*?[}|,]', title).group().split('=')[1].replace('}','').replace(',','')
+    except:
+        return ''
